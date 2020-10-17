5c0d2a47e03858e8a5d5585e37559b3feda547d5
Chloe Atwood <atwoodksw@gmail.com>
2020-10-17 00:46:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # define colors array
 colors = ["red" , "orange" , "yellow" , "green" , "blue" , "indigo" , "violet"]
-# define display_rainbow method with arguement
+# define display_rainbow method with arguement corresponding to array on line 1
 def display_rainbow(colors)
+#display_rainbow method puts string interpolated with sequential calls to array on line 1
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+#end definition of display_rainbow method
 end
